7aa6dc726f2f6e057d98dadf5e7e1b225b994d72
Angelina Anto <148966352+Angelina-Anto@users.noreply.github.com>
2026-09-17 14:14:20 +0530

    Refactor input handling and prediction logic

--- app.py
+++ app.py
@@ -1,19 +1,44 @@
 import streamlit as st
 import joblib
 
-model=joblib.load("multilinear_regression_house_price.pkl")
+model = joblib.load("multilinear_regression_house_price.pkl")
+
 st.title("House Price Prediction")
-area=st.number_input("Enter Area:", min_value=600.0 ,max_value=3000.0, value=600.0)
-bedrooms=st.number_input("Enter no of bedrooms:", min_value=1 ,max_value=4, value=3)
-floors=st.number_input("Enter no of floors:", min_value=0 ,max_value=10, value=1)
+
+area = st.number_input(
+    "Enter Area:",
+    min_value=600.0,
+    max_value=3000.0,
+    value=600.0
+)
+
+bedrooms = st.number_input(
+    "Enter no of bedrooms:",
+    min_value=1,
+    max_value=4,
+    value=3
+)
+
+floors = st.number_input(
+    "Enter no of floors:",
+    min_value=0,
+    max_value=10,
+    value=1
+)
+
+result = st.empty()
 
 if st.button("Predict"):
-   if bedrooms < 1 or bedrooms > 4:
-     st.error("Please enter the number of bedrooms between 1 and 4.")
-   elif floors < 0 or floors > 10:
-     st.error("Please enter the number of floors between 0 and 10.")
-   elif area < 600.0 or area > 3000.0:
-     st.error("Please enter the area between 600 and 3000.")
-   else:  
-    prediction=model.predict([[area,bedrooms,floors]])
-    st.success(f"Predicted price: {prediction[0]:.2f} Lakhs")
+
+    if bedrooms < 1 or bedrooms > 4:
+        result.error("Please enter the number of bedrooms between 1 and 4.")
+
+    elif floors < 0 or floors > 10:
+        result.error("Please enter the number of floors between 0 and 10.")
+
+    elif area < 600.0 or area > 3000.0:
+        result.error("Please enter the area between 600 and 3000.")
+
+    else:
+        prediction = model.predict([[area, bedrooms, floors]])
+        result.success(f"Predicted price: {prediction[0]:.2f} Lakhs")
